8eae0f0c2113435e6a98c18e5e46f8ac9c53b0ef
AEONZIBANITUM <jp031096@hotmail.com>
2026-07-26 00:53:59 -0600

    feat: agregar inicialización de arreglo dinámico

--- Practica3/SimuladorHeap/Program.cs
+++ Practica3/SimuladorHeap/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class SimuladorHeap
+{
+    // MAIN: punto de entrada, vive en el Stack
+    static void Main(string[] args)
+    {
+        Console.Write("¿Cuántos elementos? ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        // La REFERENCIA 'arreglo' vive en el Stack
+        // El OBJETO arreglo vive en el Heap
+        string[] arreglo = InicializarArreglo(n);
+    }
+
+    // Crea y retorna una nueva referencia al Heap
+    static string[] InicializarArreglo(int n)
+    {
+        string[] temp = new string[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Elemento [{i}]: ");
+            temp[i] = Console.ReadLine()!;
+        }
+
+        return temp; // retorna la referencia del Heap
+    }
+}
